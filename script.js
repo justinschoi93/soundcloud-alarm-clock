ea99bdf33d68fe19b0a2c57c1464054248d9f56d
Justin 석원 Choi <justinschoi93@gmail.com>
2023-12-20 18:29:24 -0800

    blueprints

---
+++ script.js
@@ -0,0 +1,10 @@
+// Landing Page
+// - Login/Logout button
+// - Clock
+
+// Dashboard
+// - Timezone select menu
+// - Playlist select menu
+// - Track select menu
+// - Music Player
+// - Alarm Clock

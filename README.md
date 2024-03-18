19d68db9441974f02a65820db40f7e3955e7c4fd
Justin <justinschoi93@gmail.com>
2024-03-18 16:04:50 -0700

    clock working

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # SoundCloudAlarmClock
+
+Landing Page
+- Login/Logout button
+- Clock
+
+Dashboard
+- Timezone select menu
+- Playlist select menu
+- Track select menu
+- Music Player
+- Alarm 
--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-// Landing Page
-// - Login/Logout button
-// - Clock
+$(document).ready(function () {
+    function updateClock () {
+        const date = new Date();
+        let h = date.getHours();
+        const m = date.getMinutes();
+        const s = date.getSeconds();
+        let ampm = 'am';
+        
+        
+        if (h > 12) {
+            h -= 12;
+            ampm = "pm";
+        } else if ( h === 0) {
+            h = 12;
+        }
 
-// Dashboard
-// - Timezone select menu
-// - Playlist select menu
-// - Track select menu
-// - Music Player
-// - Alarm 
+        // const hPadded = (h < 10) ? "0" + h : h;
+        const mPadded = (m < 10) ? "0" + m : m;
+        const sPadded = (s < 10) ? "0" + s : s;
 
-date = Date();
-console.log(date);
+        // Digital
+        $("#time-display").text(`${h}:${mPadded}:${sPadded} ${ampm}`);
+
+        // Analog
+        // $("#hour").css("transform", `rotate(${h * 30 + m / 2}deg)`);
+        // $("#minute").css("transform", `rotate(${m * 6}deg)`);
+        // $("#second").css("transform", `rotate(${s * 6}deg)`);
+    }
+    
+    setInterval(updateClock, 1000);
+    updateClock();
+});
